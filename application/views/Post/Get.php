6517e271e704a0833e4039d60fb1f765572f8ee5
Byron Peña <byronpenna@gmail.com>
2017-04-14 22:24:58 -0600

    Puesta de menu principal

--- application/views/Post/Get.php
+++ application/views/Post/Get.php
@@ -8,9 +8,32 @@
 </head>
 <body>
 	<div class="row marginNull divHead">
-		
+		<div class="menuPrincipal">
+			<ul>
+				<li>
+					<a href="http://www.ritsasv.com">Inicio</a>
+				</li>
+				<li>
+					<a href="http://www.ritsasv.com/cursos-gratuitos/">Cursos gratuitos</a>
+				</li>
+				<li>
+					<a href="http://www.ritsasv.com/donaciones/">Donaciones</a>
+				</li>
+				<li>
+					<a href="http://www.ritsasv.com/electronica/">Electronica</a>
+				</li>
+			</ul>
+		</div>
 	</div>
 	<div class="col-lg-3">
+		<div class="row anuncioNavegacion">
+			<!-- Prueba2 -->
+			<ins class="adsbygoogle"
+			     style="display:block"
+			     data-ad-client="ca-pub-7161297069626668"
+			     data-ad-slot="8550651839"
+			     data-ad-format="auto"></ins>
+		</div>
 		<div class="navegacion">
 			<div class="titulo">
 				<h3>Navegacion</h3>
@@ -29,17 +52,30 @@
 				<h2><?php echo $datos->post->title ?> </h2>		
 			</div>
 			<div class="cuerpo">
+				<div class="row marginNull">
+					<!-- Contenido Arriba -->
+					<ins class="adsbygoogle"
+					     style="display:block"
+					     data-ad-client="ca-pub-7161297069626668"
+					     data-ad-slot="9329381030"
+					     data-ad-format="auto"></ins>
+						
+				</div>
+				
 				<?php echo $datos->post->content ?>
+
+				<div class="row marginNull anuncio">
+					<ins class="adsbygoogle"
+				     style="display:block"
+				     data-ad-format="autorelaxed"
+				     data-ad-client="ca-pub-7161297069626668"
+				     data-ad-slot="2283792231"></ins>	
+				</div>
 			</div>
 		</div>
-		<ins class="adsbygoogle"
-	     style="display:block"
-	     data-ad-format="autorelaxed"
-	     data-ad-client="ca-pub-7161297069626668"
-	     data-ad-slot="2283792231"></ins>	
+		
 	</div>
 	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-	
 	<script>
 	     (adsbygoogle = window.adsbygoogle || []).push({});
 	</script>
