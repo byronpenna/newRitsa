f45576a350db240f91078275b245b58f1debcc7d
Byron Peña <byronpenna@gmail.com>
2017-04-14 21:03:31 -0600

    estilo

--- application/views/Post/Get.php
+++ application/views/Post/Get.php
@@ -2,8 +2,46 @@
 <html>
 <head>
 	<title>Titulo</title>
+	<?php
+		$this->load->view("parts/head.php");
+	?>
 </head>
 <body>
-	<h3><?php echo $datos->post->title ?> </h3>
+	<div class="row marginNull divHead">
+		
+	</div>
+	<div class="col-lg-3">
+		<div class="navegacion">
+			<div class="titulo">
+				<h3>Navegacion</h3>
+				<div class="row marginNull menuNavegacion">
+					<ul>
+						<li>Compra materiales de alta calidad</li>
+						<li>Realiza siempre una simulación</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="col-lg-9">
+		<div class="contenedorCuerpo">
+			<div class="titulo">
+				<h2><?php echo $datos->post->title ?> </h2>		
+			</div>
+			<div class="cuerpo">
+				<?php echo $datos->post->content ?>
+			</div>
+		</div>
+		<ins class="adsbygoogle"
+	     style="display:block"
+	     data-ad-format="autorelaxed"
+	     data-ad-client="ca-pub-7161297069626668"
+	     data-ad-slot="2283792231"></ins>	
+	</div>
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	
+	<script>
+	     (adsbygoogle = window.adsbygoogle || []).push({});
+	</script>
 </body>
 </html>
---
+++ application/views/parts/head.php
@@ -0,0 +1,8 @@
+<!-- meta -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- css -->
+	<!-- terceros -->
+		<link rel="stylesheet" type="text/css" href=<?php echo base_url("content/css/terceros/bootstrap/css/bootstrap.min.css") ?> >
+	<!-- propios -->
+		<link rel="stylesheet" type="text/css" href=<?php echo base_url("content/css/universales/style.css") ?> >
