7d10bb045b3bbaefb5d2d023b119a4e8f933a936
Byron Peña <byronpenna@gmail.com>
2017-12-17 17:16:01 -0600

    estilo

--- application/views/Post/GetPosts.php
+++ application/views/Post/GetPosts.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title><?php $post->_postTitle; ?></title>
+	<title><?php echo $post->_postTitle; ?></title>
 	<?php
 		$this->load->view("parts/head.php");
 	?>
@@ -26,6 +26,71 @@
 				</li>
 			</ul>
 		</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-3">
+			<div class="row">
+				
+			
+				<div class="row anuncioNavegacion">
+					<!-- Prueba2 -->
+					<ins class="adsbygoogle"
+					     style="display:block"
+					     data-ad-client="ca-pub-7161297069626668"
+					     data-ad-slot="8550651839"
+					     data-ad-format="auto"></ins>
+				</div>
+				<div class="navegacion">
+					<div class="titulo">
+						<h3>Navegacion</h3>
+						<div class="row marginNull menuNavegacion">
+							<ul>
+								<li>Compra materiales de alta calidad</li>
+								<li>Realiza siempre una simulación</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-9">
+			<div class="contenedorCuerpo row">
+				<div class="titulo">
+					<h2><?php echo $post->_postTitle ?> </h2>		
+				</div>
+				<div class="cuerpo">
+					<div class="row marginNull">
+						<!-- Contenido Arriba -->
+						<ins class="adsbygoogle"
+						     style="display:block"
+						     data-ad-client="ca-pub-7161297069626668"
+						     data-ad-slot="9329381030"
+						     data-ad-format="auto"></ins>
+							
+					</div>
+					
+					<?php echo $post->_postContent ?>
+
+					<div class="row marginNull anuncio">
+						<ins class="adsbygoogle"
+					     style="display:block"
+					     data-ad-format="autorelaxed"
+					     data-ad-client="ca-pub-7161297069626668"
+					     data-ad-slot="2283792231"></ins>	
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<script>
+	     (adsbygoogle = window.adsbygoogle || []).push({});
+	</script>
+	<div class="row">
+		<pre>
+			<?php print_r($post); ?>
+		</pre>	
 	</div>
 	
 </body>
