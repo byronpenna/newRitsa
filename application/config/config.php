585953bb8935f25d85288ffe93b5ab9d7fcf0dc4
Byron i7 <byronpenna@gmail.com>
2018-10-13 19:47:29 -0500

    cambio

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/newritsa/';//http://ajedrezpolitico.net/
+$config['base_url'] = 'http://localhost:8080/newritsa/';//http://new.ritsasv.com
 
 /*
 |--------------------------------------------------------------------------
